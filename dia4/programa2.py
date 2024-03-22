54e24f0f70fe881653a0e93fbaff51158e759833
josehermosillaa <jose.hermosillaa@usach.cl>
2024-03-22 18:19:10 -0300

    dia 4 finish

--- dia4/programa2.py
+++ dia4/programa2.py
@@ -1,7 +1,7 @@
 from medicamentos3 import Medicamento
-
+print("*********************BIENVENIDO AL SISTEMA DE FARMACIAS**************\n")
 opcion_ingreso = int(input(f"""
-Desea agregar otro medicamento?
+Desea agregar un medicamento?
 1. Si
 2. No
 """))
@@ -16,23 +16,38 @@ while opcion_ingreso == 1:
     if m in ingresados:
         indice = ingresados.index(m)
         ingresados[indice] += m
+        ingresado = ingresados[indice]
+        print(f""" ********* DATOS DEL MEDICAMENTO {ingresado.nombre} ***********
+        PRECIO BRUTO: ${ingresado.precio_bruto}""")
+        if m.descuento:
+            print(f"DESCUENTO: ${ingresado.descuento * 100}%")
+        print(f"PRECIO FINAL: ${ingresado.precio_final}")
+        print(f"STOCK: {ingresado.stock}")
+
+        print(f"La farmacia cuenta con {len(ingresados)} medicamentos")
+        # print(ingresados[0].stock)
+        opcion_ingreso = int(input(f"""
+        Desea agregar otro medicamento?
+        1. Si
+        2. No
+    """))
     else:
         ingresados.append(m)
         precio_bruto = int(input("Ingrese precio bruto: "))
         m.precio = precio_bruto
 
-    print(f""" ********* DATOS DEL MEDICAMENTO {m.nombre} ***********
-    PRECIO BRUTO: ${m.precio_bruto}""")
-    if m.descuento:
-        print(f"DESCUENTO: ${m.descuento * 100}%")
-    print(f"PRECIO FINAL: ${m.precio_final}")
-    print(f"STOCK: {m.stock}")
+        print(f""" ********* DATOS DEL MEDICAMENTO {m.nombre} ***********
+        PRECIO BRUTO: ${m.precio_bruto}""")
+        if m.descuento:
+            print(f"DESCUENTO: ${m.descuento * 100}%")
+        print(f"PRECIO FINAL: ${m.precio_final}")
+        print(f"STOCK: {m.stock}")
 
-    print(f"La farmacia cuenta con {len(ingresados)} medicamentos")
-    print(ingresados[0].stock)
-    opcion_ingreso = int(input(f"""
-    Desea agregar otro medicamento?
-    1. Si
-    2. No
-"""))
+        print(f"La farmacia cuenta con {len(ingresados)} medicamentos")
+        # print(ingresados[0].stock)
+        opcion_ingreso = int(input(f"""
+        Desea agregar otro medicamento?
+        1. Si
+        2. No
+    """))
 
