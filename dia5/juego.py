a4c320e24582f7213af500f5a51397309e9f4ab8
josehermosillaa <jose.hermosillaa@usach.cl>
2024-03-22 20:31:38 -0300

    codigo

--- dia5/juego.py
+++ dia5/juego.py
@@ -17,4 +17,30 @@ opcion_orco = Personaje.mostrar_dialogo_opcion(probabilidad_ganar)
 ##aca devolvemos 1 para atacar y 2 para huir y se acaba el programa
 
 while opcion_orco == 1:
-    pass
+    numero = random.uniform(0,1)
+    # resultado = ''
+    resultado = 'G' if numero < probabilidad_ganar else 'P'
+    # if numero< probabilidad_ganar:
+    #     resultado = 'G'
+    # else:
+    #     resultado = 'P'
+    if resultado == 'G':
+        print(f"""Le has ganado al orco, Felicidades!
+        Recibiras 50 puntos de experiencia!        """)
+        p.estado = 50
+        o.estado = -30
+    elif resultado == 'P':
+        print(f"""Oh No! El Orco te ha ganado!
+        Has perdido 30 puntos de experiencia!       """)
+        p.estado = -30
+        o.estado = 50
+
+    print(p.estado)
+    print(o.estado)
+    probabilidad_ganar = p.get_probabilidad_ganar(o)
+    opcion_orco = Personaje.mostrar_dialogo_opcion(probabilidad_ganar)
+
+print('¡Has huido! El orco ha quedado atrás.')
+
+
+
