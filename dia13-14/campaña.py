ffabcd52e4ef39f42104f83d863ce334907dd1e8
josehermosillaa <jose.hermosillaa@usach.cl>
2024-04-04 19:01:48 -0300

    campania.py

--- dia13-14/campaña.py
+++ dia13-14/campaña.py
@@ -1,2 +1,25 @@
 class Campania():
-    pass
+    def __init__(self, nombre:str, fecha_inicio:date, fecha_termino:date, anuncios:list):
+        self.__nombre = nombre
+        self.__fecha_inicio = fecha_inicio
+        self.__fecha_termino = fecha_termino
+        self.__anuncios = anuncios #aqui tendre instancias de la clase anuncios y sus hijas Display, sopcial, video
+
+
+    def __str__(self):
+        cant_video = 0
+        cant_display = 0
+        cant_social = 0
+        #anuncios nos tra objetos de tipo [Video, Social, Video, Display]
+        for elemento in self.__anuncios:
+            if isinstance(elemento, Video):
+                cant_video +=1
+            elif isinstance(elemento, Display):
+                cant_display+=1
+            elif isinstance(elemento, Social):
+                cant_social += 1
+        # cant_video = len([elemento for elemento in self.anuncios if isinstance(elemento,Video)])    
+        return f"""
+        Nombre de la Campaña: {self.__nombre}
+        Anuncios: {cant_video} Video, {cant_display} Display, {cant_social} Social
+        """
